29a92570abf31a6239ea5c4fc1047f35398fa68a
myangelxe <MyMyali13@icloud.com>
2026-05-30 22:04:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+
+    const sections = document.querySelectorAll(".content-section");
+
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+
+    document.getElementById(sectionId).style.display = "block";
+
+}
